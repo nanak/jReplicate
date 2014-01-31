e9957020129e506a4c5acd5b219a7d52a9e36435
Nanak Tattyrek <nanak@tattyrek.at>
2014-01-31 01:50:00 +0100

    added database connection and database initialisation

--- src/client/DatabaseConnection.java
+++ src/client/DatabaseConnection.java
@@ -1,5 +1,123 @@
 package client;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class DatabaseConnection {
 
+	/**
+	 * Insert into table
+	 */
+	public void insert(String statement) {
+		Connection c = null;
+		Statement stmt = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:db.sqlite");
+			c.setAutoCommit(false);
+			System.out.println("Opened database successfully");
+
+			stmt = c.createStatement();
+
+			stmt.executeUpdate(statement);
+
+			stmt.close();
+			c.commit();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Records created successfully");
+	}
+
+	/**
+	 * Select Query
+	 */
+	public ArrayList<String> select(String statement) {
+		Connection c = null;
+		Statement stmt = null;
+		ArrayList<String> al = new ArrayList<String>();
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			c.setAutoCommit(false);
+			System.out.println("Opened database successfully");
+
+			stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery(statement);
+
+			String[] cache = statement.split("\\s+");
+
+			if (cache[3].toLowerCase().equals("customer")) {
+				while (rs.next()) {
+					String email = rs.getString("email");
+					String password = rs.getString("password");
+					al.add(email);
+					al.add(password);
+				}
+			}
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Operation done successfully");
+		return al;
+	}
+
+	/**
+	 * Update statement
+	 */
+	public void update(String statement) {
+		Connection c = null;
+		Statement stmt = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			c.setAutoCommit(false);
+			System.out.println("Opened database successfully");
+
+			stmt = c.createStatement();
+			stmt.executeUpdate(statement);
+			c.commit();
+
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Operation done successfully");
+	}
+
+	/**
+	 * Delete
+	 */
+	public void delete(String statement) {
+		Connection c = null;
+		Statement stmt = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			c.setAutoCommit(false);
+			System.out.println("Opened database successfully");
+
+			stmt = c.createStatement();
+			stmt.executeUpdate(statement);
+			c.commit();
+
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Operation done successfully");
+	}
 }
---
+++ src/client/InitializeDatabase.java
@@ -0,0 +1,40 @@
+package client;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class InitializeDatabase {
+
+	/**
+	 * Initialize Database
+	 */
+	public static void main(String[] args) {
+		Connection c = null;
+		Statement stmt = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:db.sqlite");
+			System.out.println("Opened database successfully");
+
+			stmt = c.createStatement();
+			String sql1 = "CREATE TABLE customer "
+					+ "(email      TEXT PRIMARY KEY     NOT NULL,"
+					+ " password   TEXT                 NOT NULL)";
+			stmt.executeUpdate(sql1);
+			
+			String sql2 = "CREATE TABLE bill "
+					+ "(ID      INT PRIMARY KEY     NOT NULL,"
+					+ " date    TEXT                NOT NULL,"
+					+ " relPath TEXT                NOT NULL)";
+			stmt.executeUpdate(sql2);
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Database was successfully created");
+	}
+	
+}
