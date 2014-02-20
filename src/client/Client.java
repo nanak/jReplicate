bc494dde9635d2d70fd675e58e643d81e803b0ac
Viktor Kiss <viktor.kiss@student.tgmac.at>
2014-02-20 21:11:46 +0100

    changed input method

--- src/client/Client.java
+++ src/client/Client.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
 
@@ -13,67 +14,67 @@ public class Client {
 	private int port;
 	private int reveivingPort;
 	private String ip;
-	
+
 	public Client(String name) {
 		this.name = name;
+		this.port = 50000;
+		this.reveivingPort = 50001;
+		this.ip = "127.0.0.1";
 	}
-	
+
 	public static void main(String args[]) {
-		Client c = new Client(args[0]);
+		Client c = null;
 		try {
-			c.cmd = new CmdController(c);
-		} catch (IOException e) {
-			System.out.println("The was an error with the parameters! Please check your input and start the program again!");
+			c = new Client(args[0]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out
+					.println("The was an error with the parameters! Please check your input and start the program again!");
 			System.exit(0);
 		}
-		
-		c.port = 50000;
-		c.reveivingPort = 50001;
+
 		Socket socket = null;
+
 		try {
 			c.port = Integer.parseInt(args[2]);
-			c.reveivingPort = Integer.parseInt(args[3]);
 			socket = new Socket(args[1], c.port);
-			
+			c.reveivingPort = Integer.parseInt(args[3]);
+			c.cmd = new CmdController(c);
+
 		} catch (NumberFormatException | IOException e) {
 			if (e.getClass().equals(NumberFormatException.class)) {
-				System.out.println("WARNING: The port is set to 50000 because the second argument was invailed! \n If you want to use another port please restart the programm!");
+				System.out
+						.println("WARNING: The second / third argument was invailed! \n Please restart the programm!");
 			} else if (e.getClass().equals(IOException.class)) {
-				System.out.println();
+				System.out.println("ERROR");
 			}
 		}
-		
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        boolean goOn = true;
-        String line = null;
-        try {
-            while( (line = br.readLine())!=null && goOn){
-                System.out.println("StdIn: " + line); 
-                if (line.toUpperCase().equals("EXIT")) {
-                	goOn = false;
-                	System.out.println("Thread will be terminated!");
-                } else if (c.cmd.checkInput(line)) {
-                	System.out.println("The command was executed successfully!");
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+		Scanner sc = new Scanner(new InputStreamReader(System.in));
+		boolean goOn = true;
+		String line = null;
+		while ((line = sc.nextLine()) != null && goOn) {
+			System.out.println("StdIn: " + line);
+			if (line.toUpperCase().equals("EXIT")) {
+				goOn = false;
+				System.out.println("Thread will be terminated!");
+			} else if (c.cmd.checkInput(line)) {
+				System.out.println("The command was executed successfully!");
+			}
+		}
 	}
-	
+
 	public String getName() {
 		return this.getName();
 	}
-	
+
 	public String getIP() {
 		return this.ip;
 	}
-	
+
 	public int getPort() {
 		return this.port;
 	}
-	
+
 	public int getReiceivingPort() {
 		return this.reveivingPort;
 	}
